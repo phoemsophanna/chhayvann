e69423f55938798e43be784c5b39cd4e37fd87a0
phoemsophanna <phannaphoem@gmail.com>
2026-03-09 14:44:17 +0700

    Update some code

--- components/sections/home1/About.tsx
+++ components/sections/home1/About.tsx
@@ -146,7 +146,7 @@ export default function About({homepage}:any) {
             <RoundTextScript />
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-6 col-lg-6">
+                    <div className="col-xl-6 col-lg-6 mb-4">
                         <div className="about-style1__content wow fadeInRight animated" data-wow-delay="00ms"
                             data-wow-duration="1500ms">
                             <div className="sec-title">
