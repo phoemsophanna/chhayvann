dc58346cf0c3573b894d22c29a5514a0a58ccf46
phoemsophanna <phannaphoem@gmail.com>
2026-09-15 15:13:43 +0700

    Update some code

--- components/sections/InnerPage/Partners.tsx
+++ components/sections/InnerPage/Partners.tsx
@@ -34,60 +34,13 @@ export default function Partners({about, partner}: any) {
                         <FadeIn direction="down" delay={0.0}>
                         <div className="partner-style1__brand">
                             <ul className="clearfix">
-                                <li>
-                                    <Link href={partner[0]?.link ? partner[0]?.link : "#"} target={partner[0]?.link ? "_blank" : ""}>
-                                        <img src={`${api.FILE_URL}${partner[0]?.image}`} alt="Image" width={285} height={150} />
-                                    </Link>
-                                </li>
-                                {
-                                    partner[1] ? (
-                                        <li>
-                                            <Link href={partner[1]?.link ? partner[1]?.link : "#"} target={partner[1]?.link ? "_blank" : ""}>
-                                                <img src={`${api.FILE_URL}${partner[1]?.image}`} alt="Image" width={285} height={150} />
-                                            </Link>
-                                        </li>
-                                    ) : ""
-                                }
-                            </ul>
-                            <ul className="middle-box clearfix">
-                                {
-                                    partner[2] ? (
-                                        <li>
-                                            <Link href={partner[2]?.link ? partner[2]?.link : "#"} target={partner[2]?.link ? "_blank" : ""}>
-                                                <img src={`${api.FILE_URL}${partner[2]?.image}`} alt="Image" width={285} height={150} />
-                                            </Link>
-                                        </li>
-                                    ) : ""
-                                }
-                                <li className="counting-box" style={partner?.length < 2 ? {maxWidth: "100%"} : {}}>
-                                    <div className="count-box text-center">
-                                        <div className="odometer-counting">
-                                            <h2 className="odometer"><CountUp end={about?.valuePartner.replace("+","")} duration={2.5} /></h2>
-                                            <span>+</span>
-                                        </div>
-                                        <p>Industry Leaders <br />with Us</p>
-                                    </div>
-                                </li>
-                                {
-                                    partner[3] ? (
-                                        <li>
-                                            <Link href={partner[3]?.link ? partner[3]?.link : "#"} target={partner[3]?.link ? "_blank" : ""}>
-                                                <img src={`${api.FILE_URL}${partner[3]?.image}`} alt="Image" width={285} height={150} />
-                                            </Link>
-                                        </li>
-                                    ) : ""
-                                }
-                            </ul>
-                            <ul className="clearfix">
                                 {
                                     partner?.map((q:any, i:any) => {
-                                        if(i > 3) {
-                                            return <li key={i}>
-                                                <Link href={q?.link ? q?.link : "#"} target={q?.link ? "_blank" : ""}>
-                                                    <img src={`${api.FILE_URL}${q?.image}`} alt="Image" width={285} height={150} />
-                                                </Link>
-                                            </li>;
-                                        }
+                                        return <li key={i}>
+                                            <Link href={q?.link ? q?.link : "#"} target={q?.link ? "_blank" : ""}>
+                                                <img src={`${api.FILE_URL}${q?.image}`} alt="Image" width={285} height={150} />
+                                            </Link>
+                                        </li>;
                                     })
                                 }
                             </ul>
